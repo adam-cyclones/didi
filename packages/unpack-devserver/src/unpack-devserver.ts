a3918a7f1de6f4e4949272e734090215af6d2490
Adam Crockett <adam.crockett@forgerock.com>
2020-07-02 10:39:16 +0100

    The dev-server running in koa at the moment, uses lib unpack and provides a few extra arguments for server specific things, proxy and https is not yet supported but is planned.

---
+++ packages/unpack-devserver/src/unpack-devserver.ts
@@ -0,0 +1,58 @@
+import Koa from 'koa';
+import { IDevServerArgs } from './types/types';
+import minimist from 'minimist';
+import { promises } from "fs";
+import { resolve, extname } from 'path';
+import mime from 'mime-types';
+import { parse } from 'url';
+
+const { readFile } = promises;
+
+const argv: Partial<IDevServerArgs> = minimist(process.argv.slice(2))
+
+const readWithMime = async (ctx: Koa.Context, path: string) => {
+  ctx.body = await readFile(path, 'utf8');
+  ctx.type = mime.lookup(extname(parse(ctx.request.url).pathname as string));
+  console.log(ctx.type, )
+}
+
+export const devServer = async ({
+  host,
+  index,
+  https,
+  port,
+  reload,
+  root,
+  verbose
+}: Partial<IDevServerArgs>) => {
+  const dev = new Koa();
+
+  if (root && index) {
+    dev.use(async (ctx) => {
+      if (ctx.request.url === '/') {
+        try {
+          await readWithMime(ctx, resolve(root, index));
+        } catch (e) {
+          ctx.body = '500';
+        }
+      } else {
+        console.log(root, resolve(root, parse(ctx.request.url.replace('/', '')).pathname as string))
+        await readWithMime(ctx, resolve(root, parse(ctx.request.url.replace('/', '')).pathname as string));
+        console.log(ctx.type, ctx.request.url)
+      }
+    });
+    dev.listen(port);
+  }
+}
+
+if (Object.keys(argv).length > 1) {
+  devServer({
+    host: argv.host,
+    index: argv.index,
+    https: argv.https,
+    port: argv.port,
+    reload: argv.reload,
+    root: argv.root,
+    verbose: argv.verbose,
+  });
+}
