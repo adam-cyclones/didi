62f3daa489387e5bd3105f216f0824d13809d63b
Adam Crockett <adam.crockett@forgerock.com>
2020-07-14 11:47:40 +0100

    patch switched to no seperate repo for dist code, its to complex to handle in my mind

--- release/didi-lib/dist/utils/removeCoreModules.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.removeCore = void 0;
-exports.removeCore = function (targetModules) {
-    return targetModules.filter(function (mod) { return !mod.isCore; });
-};
